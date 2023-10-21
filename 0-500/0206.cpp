89d67f56bd49b62a5411c567322584befff4c24c
yangsx <380129340@qq.com>
2023-10-21 16:31:29 +0800

    finish 0206.cpp recursion

--- 0-500/0206.cpp
+++ 0-500/0206.cpp
@@ -22,3 +22,36 @@ struct ListNode* reverseList(struct ListNode* head){
     }
     return node;
 }
+
+// 递归做法
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* new_head;
+    ListNode* recursive_reverse(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        ListNode* next = head->next;
+        if (next == nullptr) {
+            new_head = head;
+            return nullptr;
+        }
+        recursive_reverse(next);
+        next->next = head;
+        return head;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* tail = recursive_reverse(head);
+        if (tail != nullptr) tail->next = nullptr;
+        return new_head;
+    }
+};
