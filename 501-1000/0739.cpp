de2342fcae88f78277b1871a03982474f696f877
yangsx <380129340@qq.com>
2023-10-31 18:29:32 +0800

    finish 0739

--- 501-1000/0739.cpp
+++ 501-1000/0739.cpp
@@ -14,3 +14,20 @@ public:
         return maxtem;
     }
 };
+
+// finished in 2023.10.31
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        vector<int> ans(temperatures.size());
+        for (int i = 0; i < temperatures.size(); ++i) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
